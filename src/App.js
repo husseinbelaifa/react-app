239cd0ceebadfd678a83c534f234a0a0e5c2ac0d
Hocine Belaifa <belaifahocine@gmail.com>
2019-02-07 15:45:55 +0100

    react application

--- src/App.js
+++ src/App.js
@@ -2,26 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Person from './Person/Person'
 class App extends Component {
+  state={
+    persons:[
+      { name:'max',age:28},
+      {name:'mamu',age:29}  
+    ]
+  } 
+  
+  switchNameHandler=(newname)=>{
+    // console.log('was Clicked')
+    // this.state.persons[0].name='maximulum' 
+    this.setState({ 
+      persons:[
+        { name:newname,age:28},
+        {name:'mamu',age:29}  
+      ] 
+    })
+  }
+  nameChangerHandler=(event)=>{
+    this.setState({
+      persons:[
+        { name:'max',age:28},
+        {name:event.target.value,age:29}  
+      ] 
+    }
+ 
+    )
+  }
   render() {
+    const style={
+      backgroundColor:'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Hello,this my first react app</h1>
+      <h2>this subtitle</h2>
+      <button style={style}
+      onClick={() => this.switchNameHandler('Maximulum')} 
+      type="">
+      Switch Name {this.state.persons[0].name}</button>
+      <Person click={this.switchNameHandler.bind(this,'max')}
+       name={this.state.persons[0].name} 
+       age={this.state.persons[0].age}/>
+      <Person 
+      click={this.switchNameHandler.bind(this,'mx')}   
+      name={this.state.persons[1].name} 
+      age={this.state.persons[1].age}
+      changed={this.nameChangerHandler}>
+      My Hobbies:Racing</Person>
       </div>
     );
+
+    // return (React.createElement('div',{className:'App'},React.createElement('h1',null,'Hello,this my first react app')));
   }
 }
 
